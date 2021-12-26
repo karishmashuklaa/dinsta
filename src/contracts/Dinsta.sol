3ae171ef8902fa1701db41b1ae0fffb9ba5e3156
karishmas6 <carishmashukla28@gmail.com>
2021-12-26 19:22:33 +0530

    add contracts

--- src/contracts/Dinsta.sol
+++ src/contracts/Dinsta.sol
@@ -23,6 +23,14 @@ contract Dinsta {
         address payable author
     );
 
+    event ImageTipped(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
     // create images
     function uploadImage(string memory _imgHash, string memory _description) public {
 
@@ -37,5 +45,24 @@ contract Dinsta {
 
         // trigger the event
         emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // tip images
+    function tipImageOwner(uint _id) public payable {
+        // fetch image
+        Image memory _image = images[_id];
+
+        address payable _author = _image.author;
+
+        // pay author by sending ether
+        address(_author).transfer(msg.value);
+
+        // increment tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        // update the image
+        images[_id] = _image;
+
+        emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
     }
 }
